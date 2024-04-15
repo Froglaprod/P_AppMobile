b806a752b63db8b493dab6f459be89c31ad4eed7
Mathis Botteau <123393731+Froglaprod@users.noreply.github.com>
2024-04-15 12:50:46 +0200

    Creation du projet

---
+++ Application/LovBooks/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LovBooks
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Application/LovBooks/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace LovBooks
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
